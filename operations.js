e78b98d5bf7979ab720fe93de04f63f7487c8e57
chronosx7 <abda7kado@hotmail.com>
2021-05-24 18:48:38 -0500

    Update operations.js
    Added doc comments to add function

--- operations.js
+++ operations.js
@@ -1,3 +1,9 @@
+/**
+ * Summs all of the elements in an array and returns the result
+ * 
+ * @param {array} operands 
+ * @returns Number sum of elements within the operands array
+ */
 function add(operands){
     return operands.reduce((total, operand) => total + operand)
 }
